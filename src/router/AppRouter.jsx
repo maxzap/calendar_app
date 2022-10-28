bf9c463a151e005c332a8216586005abc2e44bbc
Maximiliano Zapata <maxzap@gmail.com>
2022-10-28 07:28:19 -0300

    Adding navbar on calendar page + Adding login & registration component and style using icons from font awesome

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,7 +4,8 @@ import { CalendarPage } from "../calendar";
 
 export const AppRouter = () => {
 
-  const authStatus = 'not-authenticated';
+  // const authStatus = 'not-authenticated';
+  const authStatus = 'authenticated';
 
   return (
     <Routes>
