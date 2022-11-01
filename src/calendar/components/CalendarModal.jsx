f2aed1c702f16305f984baebc608e642b362b5f5
Maximiliano Zapata <maxzap@gmail.com>
2022-10-31 23:56:09 -0300

    setting date picker + internationalizartion + setting form data on modal

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -1,5 +1,11 @@
+import { addHours } from "date-fns";
 import { useState } from "react";
 import Modal from "react-modal";
+import DatePicker, { registerLocale } from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import es from 'date-fns/locale/es';
+
+registerLocale('es', es);
 
 const customStyles = {
     content: {
@@ -20,6 +26,27 @@ export const CalendarModal = () => {
 
     const [isOpen, setIsOpen] = useState(true);
 
+    const [formValues, setFormValues] = useState({
+      title: 'Maximiliano',
+      notes: 'Zapata',
+      start: new Date(),
+      end: addHours(new Date(), 2),
+    });
+
+    const onInputChanged = ({ target }) => {
+      setFormValues({
+        ...formValues,
+        [target.name]: target.value
+      })
+    }
+
+    const onDateChanged = ( event, changing ) => {
+      setFormValues({
+        ...formValues,
+        [changing]: event
+      })
+    }
+
     const onCloseModal = () => {
         console.log('cerrando modal');
         setIsOpen( false );
@@ -35,9 +62,73 @@ export const CalendarModal = () => {
         overlayClassName="modal-fondo"
         closeTimeoutMS={ 200 }
     >
-        <h1>Hola Mundo</h1>
+        <h1> Nuevo evento </h1>
         <hr />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi in illo dolore debitis consectetur vitae amet aspernatur sit laboriosam similique, expedita nostrum maxime magni sint accusamus at ad beatae quos!</p>
+        <form className="container">
+            <div className="form-group mb-2">
+                <label>Fecha y hora inicio</label>
+                <DatePicker 
+                  selected={ formValues.start }
+                  onChange={ (event) => onDateChanged( event, 'start' ) }
+                  className="form-control"
+                  dateFormat="Pp"
+                  showTimeSelect
+                  locale="es"
+                  timeCaption="Hora"
+                />
+            </div>
+
+            <div className="form-group mb-2">
+                <label>Fecha y hora fin</label>
+                <DatePicker
+                  minDate={ formValues.start }
+                  selected={ formValues.end }
+                  onChange={ (event) => onDateChanged( event, 'end' ) }
+                  className="form-control"
+                  dateFormat="Pp"
+                  showTimeSelect
+                  locale="es"
+                  timeCaption="Hora"
+                />
+            </div>
+
+            <hr />
+            <div className="form-group mb-2">
+                <label>Titulo y notas</label>
+                <input 
+                    type="text" 
+                    className="form-control"
+                    placeholder="Título del evento"
+                    name="title"
+                    autoComplete="off"
+                    value={ formValues.title }
+                    onChange={ onInputChanged }
+                />
+                <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+            </div>
+
+            <div className="form-group mb-2">
+                <textarea 
+                    type="text" 
+                    className="form-control"
+                    placeholder="Notas"
+                    rows="5"
+                    name="notes"
+                    value={ formValues.notes }
+                    onChange={ onInputChanged }
+                ></textarea>
+                <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+            </div>
+
+            <button
+                type="submit"
+                className="btn btn-outline-primary btn-block"
+            >
+                <i className="far fa-save"></i>
+                <span> Guardar</span>
+            </button>
+
+        </form>
     </Modal>
   )
 }
