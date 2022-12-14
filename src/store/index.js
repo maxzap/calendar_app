459c77855e23c57f9a147fa5d2bd80ea7ce2b58d
Maximiliano Zapata <maxzap@gmail.com>
2022-12-14 01:12:01 -0300

    configure autth slice and reducers.

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
-
-
+export * from './auth/authSlice';
 export * from './ui/uiSlice';
 export * from './calendar/calendarSlice';
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,10 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import { uiSlice, calendarSlice } from "./";
+import { configureStore } from "@reduxjs/toolkit";
+import { uiSlice, calendarSlice, authSlice } from "./";
 
 
 export const store = configureStore({
     reducer: {
+        auth: authSlice.reducer,
         calendar: calendarSlice.reducer,
         ui: uiSlice.reducer
     },
