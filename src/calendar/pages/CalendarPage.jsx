240291970537c32f3ae5de6bbc39e838959bf166
Maximiliano Zapata <maxzap@gmail.com>
2023-04-09 08:06:07 -0300

    change event colours by user

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -6,12 +6,12 @@ import { Navbar, CalendarEventBox, CalendarModal, FabAddNew, FabDelete } from ".
 
 import { localizer, getMessagesES } from '../../helpers';
 import { useEffect, useState } from 'react';
-import { useUiStore, useCalendarStore } from '../../hooks';
+import { useUiStore, useCalendarStore, useAuthStore } from '../../hooks';
 
 
 export const CalendarPage = () => {
 
-
+  const { user } = useAuthStore();
   const { openDateModal } = useUiStore();
   const { events, setActiveEvent, startLoadingEvents } = useCalendarStore();
 
@@ -19,8 +19,11 @@ export const CalendarPage = () => {
 
   const eventStyleGetter = ( event, start, end, isSelected ) => {
 
+    const isMyEvent = ( user.uid === event.user._id ) || ( user.uid === event.user.uid );
+
+    /** Creacion de un evento con colores definidos */
     const style = {
-      backgroundColor: '#347CF7',
+      backgroundColor: isMyEvent ? '#347CF7' : '#465660',
       borderRadius: '0px',
       opacity: 0.8,
       color: 'white'
